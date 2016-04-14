580c6258f086dd341c57321dfeebf210fe34e4ae
Đỗ Mạnh Quân <manhquan.do@ved.com.vn>
2016-04-14 17:15:02 +0700

    init

---
+++ app/Post.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App;
+
+use Cviebrock\EloquentSluggable\SluggableInterface;
+use Cviebrock\EloquentSluggable\SluggableTrait;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model implements SluggableInterface
+{
+    use SluggableTrait;
+
+    protected $sluggable = array(
+        'build_from' => 'title',
+        'save_to'    => 'slug',
+        'unique'          => true,
+        'on_update'       => true,
+    );
+    protected $fillable = [
+        'title',
+        'category_id',
+        'slug',
+        'desc',
+        'content',
+        'image',
+        'status',
+        'seo_title'
+    ];
+
+    /**
+     * post belong to one category.
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
+
+    /**
+     * like tags.
+     * @param $query
+     * @param $tag
+     * @return mixed
+     */
+    public function scopeTagged($query, $tag)
+    {
+        if (strlen($tag) > 2) {
+            $query->where('title', 'LIKE', '%'.$tag.'%');
+        }
+    }
+
+    public function scopePublish($query)
+    {
+        $query->where('status', true);
+    }
+
+    /**
+     * get the tags that associated with given post
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tag');
+    }
+
+    /**
+     * get the list tags of current post.
+     * @return mixed
+     */
+    public function getTagListAttribute()
+    {
+        return $this->tags->lists('name')->all();
+    }
+}
---
+++ app/Providers/ViewComposerProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Providers;
+
+use App\Category;
+use App\Post;
+use Illuminate\Support\ServiceProvider;
+
+class ViewComposerProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Using class based composers...
+        view()->composer(
+            'profile', 'App\Http\ViewComposers\ProfileComposer'
+        );
+
+        // Using Closure based composers...
+        view()->composer('dashboard', function ($view) {
+            //
+        });
+
+        view()->composer('example.composer', function ($view) {
+            $view->with('latestPosts',  Post::latest()->limit(6)->get());
+        });
+        view()->composer('frontend.header', function ($view) {
+            $view->with('headerCategories',  Category::whereNull('parent_id')->get());
+        });
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
